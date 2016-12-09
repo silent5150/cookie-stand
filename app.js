99094883fe1cf4bce271a060b0c8a17545662c5a
Cherise Mitchell <cherise.l.mitchell@gmail.com>
2016-12-08 23:24:51 -0800

    third push

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '
 
 var firstAndPike = {
   locationName: 'First and Pike',
-  minHourlyCust: 23,
-  maxHourlyCust: 65,
+  minCustPerHour: 23,
+  maxCustPerHour: 65,
   avgCookiePerCust: 6.3,
-  randomHourlyCust:[]
-  cookiesSoldHour: []
-  totalcookiesSoldDaily:0;
-  calculateRandomCustPerHour: function() {
-}
-for (var i = 0; i < hours.length; i++) {
+  randomHourlyCust:[],
+  totalCookieSoldPerHour: []
+  totalDailyCookiesSales: 0,
+  calculateRandomCustPerHour: function()
+
+console.log(this.randomCustPerHour, 'random customers per hour' = this.minCustPerHour + 1)
+
+for over this (var i = 0; i < hours.length; i++) {
 }
