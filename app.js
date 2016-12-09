17ebb8948151a8d7d33b7107109d0dea749eb104
Cherise Mitchell <cherise.l.mitchell@gmail.com>
2016-12-08 21:42:34 -0800

    third commit

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '
 
 var firstAndPike = {
   locationName: 'First and Pike',
-  minHourlyCust: 
-  maxHourlyCust:
-  avgCookieCust:
-  randomHourlyCust:
-  cookiesSoldHour:
-  cookiesSoldDaily:
+  minHourlyCust: 23,
+  maxHourlyCust: 65,
+  avgCookiePerCust: 6.3,
+  randomHourlyCust:[]
+  cookiesSoldHour: []
+  totalcookiesSoldDaily:0;
+  calculateRandomCustPerHour: function() {
+}
+for (var i = 0; i < hours.length; i++) {
 }
