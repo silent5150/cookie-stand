1c8f018fcf0122514842ce1077eb37a8ec087e08
Cherise Mitchell <cherise.l.mitchell@gmail.com>
2016-12-10 14:15:21 -0800

    first commit day7 branch3

--- app.js
+++ app.js
@@ -2,26 +2,28 @@
 
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
-var firstAndPike = {
+var FirstAndPike = {
   locationName: 'First and Pike',
   minCustPerHour: 23,
   maxCustPerHour: 65,
   avgCookiePerCust: 6.3,
   randomHourlyCust:[],
-  totalCookieSoldPerHour: []
-  totalDailyCookiesSales: 0,
+  totalCookieSoldPerHour: [],
+  totalDailyCookieSales: 0,
   calculateRandomCustPerHour: function() {
-    for over this (var i = 0; i < hours.length; i++); {
-      this.randomCustPerHour.push(math.floor(math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour);
-      console.log(this.randomCustPerHour;
 
+    for (var i = 0; i < hours.length; i++) {
+      this.randomCustPerHour.push(Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour);
+      console.log(this.randomCustPerHour);
+    }
   },
-  calcTotalCookiesSoldPerHour: function() {
-    this.totalCookieSoldPerHour.push('');
+  calcTotalCookiesPerHour: function() {
+    this.calcRandomCustPerHour();
+    for (var i = 0; i < hours.length; i++) {
+      this.totalCookiesSoldEachHour.push(Math.ceil(this.randomHourlyCust[i] * this .avgCookiePerCust));
+      console.log[this.totalCookiesSoldEachHour];
+    }
   },
-  render: function() {}
-
-//console.log(this.randomCustPerHour, 'random customers per hour' = this.minCustPerHour + 1)
-
-
-}
+  render: function() {
+  }
+};
