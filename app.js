6b59802a82b65cacf9f0eefcbb982893679f7a11
Cherise Mitchell <cherise.l.mitchell@gmail.com>
2016-12-11 09:41:30 -0800

    added sales.html page

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 'use strict';
 
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
-var storeLocations = [];
-var storeTable = document.getElementById('storeTable');
+var allStores = [];
 
-for (var = i; i < hours.length; i++) {
-
+for (var i = 0; i < hours.length; i++) {
+  var liEl = document.createElement('li');
+  liEl.textContent = hours[i];
+  allStores.appendChild(liEl);
 }
